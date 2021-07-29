0bf0a6da62c197a7f2faf243d3008a6a768bdcb5
Cameron Currie <me@cameroncurrie.net>
2021-07-29 18:06:24 -0500

    Add notes.txt

--- lib/url_shorten_web/controllers/redirect_controller.ex
+++ lib/url_shorten_web/controllers/redirect_controller.ex
@@ -1,6 +1,10 @@
 defmodule UrlShortenWeb.RedirectController do
+  @moduledoc """
+  This module handles requests to shortened URLs, redirecting them to their destination.
+  """
   use UrlShortenWeb, :controller
 
+  @doc "Redirects a slug to its destination URL."
   def redirect_slug(conn, %{"slug" => slug}) do
     case UrlShorten.Shortener.get_url_for_slug(slug) do
       nil -> put_flash(conn, :error, "Invalid slug") |> redirect(to: "/")
